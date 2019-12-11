4723972ed043f042612b80c35c8cd9fffd8241a9
neiroxgod <57998374+neiroxgod@users.noreply.github.com>
2019-12-11 18:28:40 +0300

    Add files via upload

---
+++ distortion test/app.js
@@ -0,0 +1,10 @@
+new hoverEffect({
+    parent: document.querySelector('.container'),
+    intensity1: 0.3,
+    intensity2: 0.5,
+    speedIn: 1,
+    speedOut: 2,
+    image1: 'santa.jpg',
+    image2: 'second.jpg',
+    displacementImage: 'heightMap.png',
+})
